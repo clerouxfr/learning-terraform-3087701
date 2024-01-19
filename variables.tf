45164826c5dd92bb76402d15169eb59b4862f6f9
clerouxfr <36843995+clerouxfr@users.noreply.github.com>
2024-01-19 17:56:33 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,3 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
-variable "AWS_SECRET_ACCESS_KEY" {
-  default = "Z1xPZJics9xp3pL0k3DEh9+5SquxFmp/4IEiTsqA"
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  default = "AKIAZQ3DRYMNI3MTHPQN"
-}
